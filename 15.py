1312f226bcbcce0abc1ed8a664cf18cfe6b57726
Niharika Singh <s.niharika09@gmail.com>
2018-12-22 22:40:25 -0500

    progress save part 1

--- 15.py
+++ 15.py
@@ -2,8 +2,8 @@ import copy, collections, pdb
 
 debug = True
 
-def bfs(orig_map, start_cell, w, h, looking_for):
-    map = copy.deepcopy(orig_map)
+def bfs(map, start_cell, w, h, looking_for):
+    check_map = [[True]*w for i in range(h)]
     current_nodes = collections.deque([[start_cell]])
     found_nodes = []
     while (len(found_nodes) == 0) and (len(current_nodes) > 0):
@@ -12,38 +12,34 @@ def bfs(orig_map, start_cell, w, h, looking_for):
             curr_path = current_nodes.popleft()
             last_node_on_path = curr_path[-1]
             # print("Checking with node:", last_node_on_path)
-            map[last_node_on_path[0]][last_node_on_path[1]] = 'x'
             # if (last_node_on_path == (1, 4)):
             #     pdb.set_trace()
             # check in reading order
-            if (last_node_on_path[0] > 0) and (map[last_node_on_path[0]-1][last_node_on_path[1]][0] in ['.', looking_for]):
+            if (last_node_on_path[0] > 0) and (check_map[last_node_on_path[0]-1][last_node_on_path[1]]) and (map[last_node_on_path[0]-1][last_node_on_path[1]][0] in ['.', looking_for]):
                 # print("Got top")
-                new_path = copy.deepcopy(curr_path)
-                new_path.append((last_node_on_path[0]-1, last_node_on_path[1]))
+                new_path = curr_path + [(last_node_on_path[0]-1, last_node_on_path[1])]
                 current_nodes.append(new_path)
                 if (map[last_node_on_path[0]-1][last_node_on_path[1]][0] == looking_for):
                     found_nodes.append(new_path)
-            if (last_node_on_path[1] > 0) and (map[last_node_on_path[0]][last_node_on_path[1]-1][0] in ['.', looking_for]):
+            if (last_node_on_path[1] > 0) and (check_map[last_node_on_path[0]][last_node_on_path[1]-1]) and (map[last_node_on_path[0]][last_node_on_path[1]-1][0] in ['.', looking_for]):
                 # print("Got left")
-                new_path = copy.deepcopy(curr_path)
-                new_path.append((last_node_on_path[0], last_node_on_path[1]-1))
+                new_path = curr_path + [(last_node_on_path[0], last_node_on_path[1]-1)]
                 current_nodes.append(new_path)
                 if (map[last_node_on_path[0]][last_node_on_path[1]-1][0] == looking_for):
                     found_nodes.append(new_path)
-            if (last_node_on_path[1] < w-1) and (map[last_node_on_path[0]][last_node_on_path[1]+1][0] in ['.', looking_for]):
+            if (last_node_on_path[1] < w-1) and (check_map[last_node_on_path[0]][last_node_on_path[1]+1]) and (map[last_node_on_path[0]][last_node_on_path[1]+1][0] in ['.', looking_for]):
                 # print("Got right")
-                new_path = copy.deepcopy(curr_path)
-                new_path.append((last_node_on_path[0], last_node_on_path[1]+1))
+                new_path = curr_path + [(last_node_on_path[0], last_node_on_path[1]+1)]
                 current_nodes.append(new_path)
                 if (map[last_node_on_path[0]][last_node_on_path[1]+1][0] == looking_for):
                     found_nodes.append(new_path)
-            if (last_node_on_path[0] < h-1) and (map[last_node_on_path[0]+1][last_node_on_path[1]][0] in ['.', looking_for]):
+            if (last_node_on_path[0] < h-1) and (check_map[last_node_on_path[0]+1][last_node_on_path[1]]) and (map[last_node_on_path[0]+1][last_node_on_path[1]][0] in ['.', looking_for]):
                 # print("Got bottom")
-                new_path = copy.deepcopy(curr_path)
-                new_path.append((last_node_on_path[0]+1, last_node_on_path[1]))
+                new_path = curr_path + [(last_node_on_path[0]+1, last_node_on_path[1])]
                 current_nodes.append(new_path)
                 if (map[last_node_on_path[0]+1][last_node_on_path[1]][0] == looking_for):
                     found_nodes.append(new_path)
+            check_map[last_node_on_path[0]][last_node_on_path[1]] = False
         # print("Current nodes:", current_nodes)
         # print("Found_nodes", found_nodes)
     if (len(found_nodes) == 0):
@@ -87,42 +83,55 @@ def find_target(map, start_cell, w, h, looking_for):
             lowest_hp = map[start_cell[0]][start_cell[1]+1][1]
             lowest_locations = [(start_cell[0], start_cell[1]+1)]
     lowest_locations.sort()
+    if (len(lowest_locations) == 0):
+        return []
     return lowest_locations[0]
 
-def turn(map):
+def turn(map, moved_last_turn, killed_last_turn):
     new_map = copy.deepcopy(map)
     width = len(map[0])
     height = len(map)
     completed_turn = False
+    moved_this_turn = False
+    killed_this_turn = False
 
     for ri in range(height):
         for ci in range(width):
             # turns are based on starting positions
             this_cell = map[ri][ci]
             if (len(this_cell) > 1):
+                # print(ri, ci)
                 looking_for = ''
                 if (this_cell[0] == 'G'):
                     looking_for = 'E'
                 else:
                     looking_for = 'G'
                 # find closest target
-                (can_move, next_move, can_attack) = bfs(new_map, (ri, ci), width, height, looking_for)
+                if (moved_last_turn or killed_last_turn):
+                    (can_move, next_move, can_attack) = bfs(new_map, (ri, ci), width, height, looking_for)
+                else:
+                    (can_move, next_move, can_attack) = (False, 0, True)
                 # move
                 if (can_move):
                     new_map[ri][ci] = '.'
                     new_map[next_move[0]][next_move[1]] = this_cell
+                    moved_this_turn = True
                 # attack
                 if (can_attack):
                     if (can_move):
                         target = find_target(new_map, (next_move[0], next_move[1]), width, height, looking_for)
                     else:
                         target = find_target(new_map, (ri, ci), width, height, looking_for)
-                    target_cell = new_map[target[0]][target[1]]
-                    if (target_cell[1] <= 3):
-                        new_map[target[0]][target[1]] = '.'
-                        map[target[0]][target[1]] = '.'
+                    if (len(target) > 0):
+                        target_cell = new_map[target[0]][target[1]]
+                        if (target_cell[1] <= 3):
+                            new_map[target[0]][target[1]] = '.'
+                            map[target[0]][target[1]] = '.'
+                            killed_this_turn = True
+                        else:
+                            new_map[target[0]][target[1]] = (target_cell[0], target_cell[1]-3)
                     else:
-                        new_map[target[0]][target[1]] = (target_cell[0], target_cell[1]-3)
+                        can_attack = False
                 completed_turn = (can_move or can_attack)
     # check if combat over
     saw_G = False
@@ -137,7 +146,7 @@ def turn(map):
         if (saw_G and saw_E):
             break
     combat_over = not (saw_G and saw_E)
-    return (new_map, combat_over, completed_turn)
+    return (new_map, combat_over, completed_turn, moved_this_turn, killed_this_turn)
 
 def print_map(map):
     line_map = ''
@@ -165,8 +174,13 @@ def game(ifilename):
             map.append(temp_list)
     combat_over = False
     turns = 0
+    moved_this_turn = True
+    killed_this_turn = False
+    if (debug):
+        print("Turn: ", turns)
+        # print_map(map)
     while (not combat_over):
-        (map, combat_over, completed_turn) = turn(map)
+        (map, combat_over, completed_turn, moved_this_turn, killed_this_turn) = turn(map, moved_this_turn, killed_this_turn)
         if (not combat_over) or (combat_over and completed_turn):
             turns += 1
         if (debug):
@@ -180,10 +194,10 @@ def game(ifilename):
     print("Turns:", turns, "Sum:", sum_hp, "Outcome:", turns * sum_hp)
     return turns * sum_hp
 
-assert game('15input3.txt') == 36334
+# assert game('15input3.txt') == 36334
 # assert game('15input2.txt') == 27730
-assert game('15input4.txt') == 39514
-assert game('15input5.txt') == 27755
-assert game('15input6.txt') == 28944
-assert game('15input7.txt') == 18740
+# assert game('15input4.txt') == 39514
+# assert game('15input5.txt') == 27755
+# assert game('15input6.txt') == 28944
+# assert game('15input7.txt') == 18740
 game('15input1.txt')
